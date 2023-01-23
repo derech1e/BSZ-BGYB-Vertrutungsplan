0d68bcae6a0a5dc5c17ca9939acf4658f1ad94ff
derech1e <nurkthomas@gmail.com>
2023-01-23 19:56:07 +0100

    Refactor vplan parsing to match new requirements

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,7 +8,7 @@ export const vplan =
         .region("europe-west1")
         .https
         .onRequest((request, response) => {
-            const url = "http://geschuetzt.bszet.de/s-lk-vw/Vertretungsplaene/vertretungsplan-bgy.pdf";
+            let baseUrl = "http://geschuetzt.bszet.de/s-lk-vw/Vertretungsplaene/V_PlanBGy/";
             const auth = Buffer.from("bsz-et-2223:sommer#22").toString("base64");
 
             const options: RequestOptions = {
@@ -17,7 +17,13 @@ export const vplan =
                 },
             };
 
-            const req = http.get(url, options, (res) => {
+            if (request.query.target == undefined) {
+                baseUrl += 'index.html';
+            } else {
+                baseUrl += request.query.target;
+            }
+
+            const req = http.get(baseUrl, options, (res) => {
                 const chunks: Array<Uint8Array> = [];
                 res.on("data", (chunk) => {
                     chunks.push(chunk);
@@ -25,16 +31,23 @@ export const vplan =
 
                 res.on("end", () => {
                     const buffer = Buffer.concat(chunks);
+                    let text = new TextDecoder().decode(buffer);
+                    const appUrl = `http://${request.header('host')}/${process.env.GCLOUD_PROJECT}/europe-west1/${process.env.FUNCTION_TARGET}`;
+                    const regexMatch = text.match(/V_DC_00[1-9]\.html|index\.html/g);
+                    regexMatch?.map((item) => {
+                        text = text.replace(item, `${appUrl}?target=${item}`);
+                    });
+                    const newBuffer = Buffer.from(text, "utf-8");
                     const stream = new Readable();
-                    stream.push(buffer);
+                    stream.push(newBuffer);
                     stream.push(null);
                     response.set({
-                        "Content-Type": "application/pdf",
-                        "Content-Length": buffer.length,
+                        "Content-Type": "text/html; charset=utf-8",
                     });
                     stream.pipe(response);
                 });
             });
+
             req.on("error", (err) => {
                 functions.logger.error(err, {structuredData: true});
             });
